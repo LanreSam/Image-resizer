e9cee6fac789df2bbcccb3875840303a014e640a
lanre_sam <ezechelanre@gmail.com>
2023-01-25 13:05:19 +0100

    added new files and codes

--- main.js
+++ main.js
@@ -1,26 +1,48 @@
 const path = require('path');
 
 const isMac = process.platform === 'darwin';
+const isDev = process.env.NODE_ENV !== 'production';
 
 const { 
     app,
-    BrowserWindow
+    BrowserWindow,
+    Menu
 } = require('electron');
+
 
 //creates the window of the desktop application
 const createMainWindow = () => {
     const mainWindow = new BrowserWindow({
         title: "Image Resizer",
-        width: 500,
+        width: isDev ? 1000 : 500,
         height: 600
     });
+    
+    //Open devtools
+    if (isDev) mainWindow.webContents.openDevTools();
 
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
+//creates the about window
+const createAboutWindow = () => {
+    const aboutWindow = new BrowserWindow({
+        title: "Image Resizer",
+        width: 300,
+        height: 300
+    });
+
+    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+
+
 //starts the application
 app.whenReady().then( () => {
     createMainWindow();
+
+    //Implement Menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
 
     //This block of code checks if a window has not being previously created before.
     app.on('activate', () => {
@@ -29,6 +51,35 @@ app.whenReady().then( () => {
         }
     });
 });
+
+//Menu template
+const menu = [
+    ...(isMac) ? [
+        {
+            label: app.name,
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow
+                }
+            ]
+        }
+    ] : [],
+    {
+        role: 'fileMenu'
+    },
+    ...(!isMac) ? [
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow
+                }
+            ]
+        }
+    ] : []
+]
 
 //checks for the platform the application is running on.
 app.on('window-all-closed', () => {
