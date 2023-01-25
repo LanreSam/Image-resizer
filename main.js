899cc1de9b4e29124c8f5aba9aae97909528b97c
lanre_sam <ezechelanre@gmail.com>
2023-01-25 11:18:31 +0100

    creating Main window

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
+const isMac = process.platform === 'darwin';
+
 const { 
     app,
     BrowserWindow
 } = require('electron');
 
+//creates the window of the desktop application
 const createMainWindow = () => {
     const mainWindow = new BrowserWindow({
         title: "Image Resizer",
@@ -10,5 +15,24 @@ const createMainWindow = () => {
         height: 600
     });
 
-    mainWindow.loadFile();
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
+
+//starts the application
+app.whenReady().then( () => {
+    createMainWindow();
+
+    //This block of code checks if a window has not being previously created before.
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+          createMainWindow()
+        }
+    });
+});
+
+//checks for the platform the application is running on.
+app.on('window-all-closed', () => {
+    if (!isMac) {
+      app.quit()
+    }
+  })
